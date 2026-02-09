2a596128c2dc8875274e1e51275cef4af0dfee1e
deeksha <deeksha1@appinventiv.com>
2026-02-09 16:01:24 +0530

    add some new concepts

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("./xyz");
 
 // const calculateSum = require('./sum');
-const obj = require("./sum");
+const obj = require("./sum"); // you can write it as well and javascript k acc. it is the best way to write
+const { x, calculateSum } = require("./sum");
 
 var name = "Namaste NodeJS";
 
@@ -11,8 +12,11 @@ var b = 20;
 console.log(name);
 // console.log(a + b);
 
-obj.calculateSum(a, b);
-console.log(obj.x);
+// obj.calculateSum(a, b); // when you destructuring it then you don't need to call it as (obj.calculateSum) you can call as directly like this just below line
+calculateSum(a,b); 
+
+// console.log(obj.x);
+console.log(x);
 
 // console.log(global); // need to deep
 // console.log(this); // need to deep
--- sum.js
+++ sum.js
@@ -1,4 +1,4 @@
-// Modules protect their variables and functions from leaking by default
+// Modules protect their variables and functions/methods from leaking by default --> ekk module me se jb tk wo module na chahe kii uske functions/variables/methods koi access kr sake tb tk wo accessible nii hota without using module.exports isiliye yha last me (module.exports) ka use kiya gya h
 
 console.log("Sum Module Executed");
 
@@ -9,4 +9,5 @@ function calculateSum(a, b) {
   console.log(sum);
 }
 
-module.exports = { x: x, calculateSum: calculateSum };
+// module.exports = { x: x, calculateSum: calculateSum }; // You can write it as well just below line
+module.exports = { x, calculateSum }; // this is the neat way to write
